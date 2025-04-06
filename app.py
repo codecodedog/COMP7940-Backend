342249693a12315657c7300de763677d7e43d600
Hinorpio_DESKTOP <hinscorpio@gmail.com>
2025-04-06 22:34:29 +0800

    add log

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from routes.user import user_bp
 from routes.property import property_bp
@@ -6,6 +7,14 @@ app = Flask(__name__)
 
 app.register_blueprint(user_bp)
 app.register_blueprint(property_bp)
+
+host = os.getenv('DB_ENDPOINT')
+port = 3306
+database = os.getenv('DB_NAME')
+username = os.getenv('DB_USERNAME')
+password = os.getenv('DB_PASSWORD')
+
+print(host, port, database, username, password)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
