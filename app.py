381d77f9488fc0177c37bbda4e347a02fb3739b6
Hinorpio_DESKTOP <hinscorpio@gmail.com>
2025-04-06 22:16:32 +0800

    0.0.0

--- app.py
+++ app.py
@@ -8,4 +8,4 @@ app.register_blueprint(user_bp)
 app.register_blueprint(property_bp)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
